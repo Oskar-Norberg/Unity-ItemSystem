a93b9dc503a600aab534dd315d6ccbf6324fdcc5
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-02-26 17:11:14 +0100

    EDIT: [Script] Make PlayerMovement good enough

--- Assets/_Project/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/_Scripts/Player/PlayerMovement.cs
@@ -4,9 +4,13 @@ using UnityEngine.InputSystem;
 namespace Project.Player
 {
     [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(PlayerInput))]
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private Transform cameraPivot;
+
+        [SerializeField] private float cameraSensitivity = 1.0f;
+        [SerializeField] private float movementSpeed = 5.0f;
         
         private Rigidbody _rigidbody;
         private Vector2 wishDirection;
@@ -14,17 +18,21 @@ namespace Project.Player
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
+            
+            // TODO: This is very temporary, should happen in a GameManager
+            Cursor.lockState = CursorLockMode.Locked;
         }
         
         private void OnMove(InputValue movementValue) {
-            Vector2 movementVector = movementValue.Get<Vector2>();
+            Vector2 movementVector = movementValue.Get<Vector2>() ;
             wishDirection.x = movementVector.x;
             wishDirection.y = movementVector.y;
         }
         
         private void OnLook(InputValue lookValue) {
-            Vector2 lookVector = lookValue.Get<Vector2>();
-            cameraPivot.Rotate(Vector3.right, lookVector.y);
+            Vector2 lookVector = Time.deltaTime * cameraSensitivity * lookValue.Get<Vector2>();
+            
+            cameraPivot.Rotate(Vector3.right, -lookVector.y);
             transform.Rotate(Vector3.up, lookVector.x);
         }
         
@@ -37,9 +45,13 @@ namespace Project.Player
             if (wishDirection == Vector2.zero)
                 return;
             
-            Vector3 moveDirection = new Vector3(wishDirection.x, 0, wishDirection.y);
+            Vector3 moveDirection = transform.rotation * new Vector3(wishDirection.x, 0, wishDirection.y);
+            Vector3 force = moveDirection.normalized * movementSpeed;
             
-            _rigidbody.linearVelocity = moveDirection;
+            if (_rigidbody.linearVelocity.magnitude > movementSpeed)
+                return;
+            
+            _rigidbody.AddForce(force, ForceMode.Acceleration);
         }
     }
 }

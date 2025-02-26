f203f7c92589b396b9b9bb31b9d27499c2342d5d
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-02-26 16:51:47 +0100

    EDIT: [Script] Set up basic fps controller

--- Assets/_Project/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/_Scripts/Player/PlayerMovement.cs
@@ -1,19 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Project.Player
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerMovement : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        [SerializeField] private Transform cameraPivot;
+        
+        private Rigidbody _rigidbody;
+        private Vector2 wishDirection;
 
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        
+        private void OnMove(InputValue movementValue) {
+            Vector2 movementVector = movementValue.Get<Vector2>();
+            wishDirection.x = movementVector.x;
+            wishDirection.y = movementVector.y;
+        }
+        
+        private void OnLook(InputValue lookValue) {
+            Vector2 lookVector = lookValue.Get<Vector2>();
+            cameraPivot.Rotate(Vector3.right, lookVector.y);
+            transform.Rotate(Vector3.up, lookVector.x);
+        }
+        
+        private void FixedUpdate() {
+            Move();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Move()
         {
-
+            if (wishDirection == Vector2.zero)
+                return;
+            
+            Vector3 moveDirection = new Vector3(wishDirection.x, 0, wishDirection.y);
+            
+            _rigidbody.linearVelocity = moveDirection;
         }
     }
 }

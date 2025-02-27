7aa7bf87a5dfc755cd78ed8f94ca0a2e6a8dc376
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-02-27 11:27:06 +0100

    ADD: [Script] InventoryUI

---
+++ Assets/_Project/_Scripts/UI/InventoryUI.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Net.Mime;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace Project.InventorySystem.UI
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI text;
+        
+        private Inventory _inventoryToDisplay;
+        private bool _isInventoryOpen;
+        
+        public void Awake()
+        {
+            _isInventoryOpen = gameObject.activeInHierarchy;
+        }
+        
+        public void ToggleInventory(Inventory inventory)
+        {
+            _isInventoryOpen = !_isInventoryOpen;
+
+
+            if (_isInventoryOpen)
+            {
+                _inventoryToDisplay = inventory;
+                
+            }
+
+            gameObject.SetActive(_isInventoryOpen);
+        }
+
+        // TODO: VERY TEMPORARY DEBUG DISPLAY
+        private void SetText(Inventory inventory)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in inventory.GetItems())
+            {
+                sb.Append(item.itemName + "\n")
+            }
+        }
+    }
+}

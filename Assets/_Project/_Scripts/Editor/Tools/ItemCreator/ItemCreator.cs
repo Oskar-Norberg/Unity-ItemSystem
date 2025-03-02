86912ba639502650821c54e7ecb1e8866dec5a22
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-02 18:40:14 +0100

    EDIT: [Script] Create Item in SubFolder

--- Assets/_Project/_Scripts/Editor/Tools/ItemCreator/ItemCreator.cs
+++ Assets/_Project/_Scripts/Editor/Tools/ItemCreator/ItemCreator.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Project.InteractableSystem;
 using UnityEditor;
 using UnityEngine;
 
@@ -15,7 +17,6 @@ namespace Project.ItemSystem.Editor.Tools
         private int _maxStackSize = 1;
 
         private int _typeSelectionIndex;
-        private int _subfolderSelectionIndex;
 
         [MenuItem("Tools/Iteam Creator")]
         public static void ShowWindow()
@@ -92,7 +93,66 @@ namespace Project.ItemSystem.Editor.Tools
 
         private void CreateItem()
         {
-            Debug.Log("item created :3 (not really)");
+            bool created = false;
+            
+            created = _isStackable ? CreateStackableItem() : CreateNonStackableItem();
+            
+            if (created)
+            {
+                GUILayout.Label("Item created successfully!");
+            }
+        }
+
+        private bool CreateNonStackableItem()
+        {
+            NonStackableItemData itemData = ScriptableObject.CreateInstance<NonStackableItemData>();
+            return CreateItemCommon(itemData as ItemData);
+        }
+        
+        private bool CreateStackableItem()
+        {
+            StackableItemData itemData = ScriptableObject.CreateInstance<StackableItemData>();
+            itemData.maxStackSize = _maxStackSize;
+            
+            return CreateItemCommon(itemData as ItemData);
+        }
+        
+        private bool CreateItemCommon(ItemData itemData)
+        {
+            var itemGameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var itemComponent = itemGameObject.AddComponent<Item>();
+            
+            itemData.name = _itemName;
+            itemData.sprite = _itemSprite;
+            itemData.prefab = itemGameObject;
+
+            AssetDatabase.CreateFolder(GetTypeSubFolder(_typeSelectionIndex), _itemName.Trim(' ').Trim());
+            string newSubFolder = GetTypeSubFolder(_typeSelectionIndex) + "/" + _itemName.Trim(' ').Trim() + "/";
+
+            // Save Item Data
+            AssetDatabase.CreateAsset(itemData, newSubFolder + _itemName + ".asset");
+            
+            var itemDataField = typeof(Item).GetField("itemData", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (itemDataField == null)
+            {
+                Debug.LogError("Could not find itemData field in Item.cs");
+                return false;
+            }
+
+            itemDataField.SetValue(itemComponent, itemData);
+            
+            PrefabUtility.SaveAsPrefabAsset(itemGameObject, newSubFolder + _itemName + ".prefab");
+            AssetDatabase.SaveAssets();
+            
+            DestroyImmediate(itemGameObject);
+            
+            return true;
+        }
+
+        private string GetTypeSubFolder(int index)
+        {
+            string[] itemTypePaths = AssetDatabase.GetSubFolders(ItemPath);
+            return itemTypePaths[index];
         }
     }
 }

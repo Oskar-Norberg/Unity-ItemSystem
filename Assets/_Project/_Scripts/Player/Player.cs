2aeed7720a5c3ec4cb1f86098d71e975dd1e28ed
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-02 15:16:25 +0100

    EDIT: [Script] Make Player a Singleton

--- Assets/_Project/_Scripts/Player/Player.cs
+++ Assets/_Project/_Scripts/Player/Player.cs
@@ -6,11 +6,37 @@ namespace Project.PlayerCharacter
     [RequireComponent(typeof(PlayerInteraction))]
     public class Player : MonoBehaviour
     {
+        public static Player Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Debug.LogWarning("Instance of " + nameof(Player) + " is null");
+                    return null;
+                }
+
+                return _instance;
+            }
+        }
+
+        private static Player _instance;
+        
         private PlayerMovement _playerMovement;
         private PlayerInteraction _playerInteraction;
     
         private void Awake()
         {
+            if (_instance == null)
+            {
+                _instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+            
             _playerMovement = GetComponent<PlayerMovement>();
             _playerInteraction = GetComponent<PlayerInteraction>();
         }

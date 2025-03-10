694e8f10e8832f3ef45803371a72ca95d6a50428
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-10 14:57:40 +0100

    ADD: [Script] Create Abstract ItemHolder base

---
+++ Assets/_Project/_Scripts/Item/ItemHolder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Project.InteractableSystem
+{
+    public abstract class ItemHolder : MonoBehaviour
+    {
+        [SerializeField] private Transform itemHolder;
+        
+        protected Item CurrentItem;
+        
+        public void Equip(Item item)
+        {
+            DestroyItem();
+            
+            item.transform.SetParent(itemHolder);
+            item.transform.localPosition = Vector3.zero;
+            
+            CurrentItem = item;
+        }
+
+        private void OnSubmit()
+        {
+            if (CurrentItem)
+                CurrentItem.Use(transform);
+        }
+
+        protected void DestroyItem()
+        {
+            if (CurrentItem)
+                Destroy(CurrentItem.gameObject);
+
+            CurrentItem = null;
+        }
+    }
+}
--- Assets/_Project/_Scripts/Player/PlayerItemHolder.cs
+++ Assets/_Project/_Scripts/Player/PlayerItemHolder.cs
@@ -1,35 +1,13 @@
-using UnityEngine;
+using Project.InteractableSystem;
 
 namespace Project.PlayerCharacter.Item
 {
-    public class PlayerItemHolder : MonoBehaviour
+    public class PlayerItemHolder : ItemHolder
     {
-        [SerializeField] private Transform itemHolder;
-        
-        private InteractableSystem.Item _currentItem;
-        
-        public void Equip(InteractableSystem.Item item)
-        {
-            DestroyItem();
-            
-            item.transform.SetParent(itemHolder);
-            item.transform.localPosition = Vector3.zero;
-            
-            _currentItem = item;
-        }
-
         private void OnSubmit()
         {
-            if (_currentItem)
-                _currentItem.Use(transform);
-        }
-
-        public void DestroyItem()
-        {
-            if (_currentItem)
-                Destroy(_currentItem.gameObject);
-
-            _currentItem = null;
+            if (CurrentItem)
+                CurrentItem.Use(transform);
         }
     }
 }

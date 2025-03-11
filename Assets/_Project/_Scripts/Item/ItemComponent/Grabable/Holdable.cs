181c298e8c84a039e1354a6782b76303a1e26dc8
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-11 13:22:20 +0100

    EDIT: [Script] Rename Equipper to Holder

--- Assets/_Project/_Scripts/Item/ItemComponent/Grabable/Holdable.cs
+++ Assets/_Project/_Scripts/Item/ItemComponent/Grabable/Holdable.cs
@@ -5,9 +5,9 @@ namespace Project.ItemSystem.Components
 {
     public class Holdable : ItemComponent
     {
-        public void Hold(Transform equipper)
+        public void Hold(Transform holder)
         {
-            if (!equipper.TryGetComponent<ItemHolder>(out var playerItemHolder))
+            if (!holder.TryGetComponent<ItemHolder>(out var playerItemHolder))
             {
                 Debug.LogWarning("Could not equip item, equipper does not have a ItemHolder component");
                 return;
